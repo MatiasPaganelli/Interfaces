5d38fb42290543daec1dc77585b5c4d226e36f0d
MatiasPaganelli <matiasandrespaganelli@gmail.com>
2019-11-17 18:39:04 -0300

    TP3 Reentrega

--- TP3/TP3Reentrega/EntregaFinal/js/interaccion.js
+++ TP3/TP3Reentrega/EntregaFinal/js/interaccion.js
@@ -7,12 +7,10 @@ let colision = false
 let roca = document.getElementById("roca"); 
 let valorRoca = 2500;
 var puntaje = 0;
-var start = false;
-
-function gameLoop(){
-    start = true;
+var contarPuntaje = false;
+gameLoop = function (){
     puntaje += 1;
-    document.getElementById('puntaje').innerHTML = "Puntaje:"+puntaje;
+    document.getElementById('puntaje').innerHTML = "Kilometros:"+puntaje;
     function update(){
         $(document).keydown(function(event){  
             if(event.key == "w"){
@@ -30,13 +28,14 @@ function gameLoop(){
             }, 800);
         }
         function traerPiedra(){
+            
             if(valorRoca >= -100){            
                 document.getElementById("roca").style.left = (valorRoca)+'px';
-                valorRoca = valorRoca - 10;
+                valorRoca = valorRoca - 12;
             }else{
                 valorRoca = 2500;
                 colision = false;
-            } 
+            }
         }
         traerPiedra();
         if(DetectarColision() && !colision){
@@ -47,18 +46,17 @@ function gameLoop(){
                 $(".dinosaurioCorrer").removeClass("dinosaurioCorrer").addClass("dinosaurioColisionado");
                 setTimeout(function(){
                     $(".dinosaurioColisionado").removeClass("dinosaurioColisionado").addClass("dinosaurioCorrer");
-                    },80);
-               
+                },80);
+                
             } else if (vidas == 1){
                 $(".vida1").hide();
                 $(".dinosaurioCorrer").removeClass("dinosaurioCorrer").addClass("dinosaurioMuerto");
                 finalizado();
                 puntaje.pause();
-                
-             
             }
             
         } 
+        
         // Funcion colision
         function DetectarColision(){
             /// "a" y "b" deben ser dos objetos HTMLElement
@@ -70,7 +68,7 @@ function gameLoop(){
                 r: a.position().left + a.width(), 
                 b: a.position().top + a.height()};
                 var b_pos =  {t : b.position().top, 
-                    l: b.position().left+259, 
+                    l: b.position().left+60, 
                     r: b.position().left + b.width(), 
                     b: b.position().top + b.height()};
                     
@@ -85,28 +83,64 @@ function gameLoop(){
                         }
                         
                     }
-                    
                 }
                 update();
-                globalID = requestAnimationFrame(gameLoop);  
-                 function finalizado(){
-                     //document.getElementById('dinosaurio').style.webkitAnimationPlayState = 'paused';
-                     document.getElementById('roca').style.webkitAnimationPlayState = 'paused'; 
-                     document.getElementById('fondo').style.webkitAnimationPlayState = 'paused';
-                     //document.getElementById("fondo").style.webkitFilter = "blur(3px)";
-                     document.getElementById('gameover').style.visibility = 'visible';
-                     document.getElementById('puntaje').style.visibility = 'hidden';
-                     document.getElementById('puntuacionFinal').innerHTML = "Puntuacion Final:" +puntaje + "puntos";
-                   
-                }
-        
+                idFrame = window.requestAnimationFrame(gameLoop);
             }
-            
-            window.requestAnimationFrame(gameLoop);  
-            if(vidas == 0){
-                cancelAnimationFrame(globalID);   
+            function finalizado(){
+                document.getElementById('dinosaurio').style.webkitAnimationPlayState = 'paused';
+                document.getElementById('roca').style.webkitAnimationPlayState = 'paused'; 
+                document.getElementById('fondo').style.webkitAnimationPlayState = 'paused';
+                document.getElementById("fondo").style.webkitFilter = "blur(8px)";
+                document.getElementById('gameover').style.visibility = 'visible';
+                document.getElementById('puntaje').style.visibility = 'hidden';
+                document.getElementById('volverajugar').style.visibility = 'visible';
+                document.getElementById('puntuacionFinal').innerHTML = "Puntaje: " +puntaje;
+                window.cancelAnimationFrame(idFrame);
+                
             }
+            function start(){
+                vidas = 3;
+                colision = false;                
+                valorRoca = 2500;
+                puntaje = 0;
+                document.getElementById('dinosaurio').style.webkitAnimationPlayState = 'running';                
+                document.getElementById('fondo').style.webkitAnimationPlayState = 'running';
+                document.getElementById('roca').style.webkitAnimationPlayState = 'running'; 
+                document.getElementById('vida1').style.visibility = 'visible'; 
+                document.getElementById('vida2').style.visibility = 'visible'; 
+                document.getElementById('vida3').style.visibility = 'visible'; 
+                window.requestAnimationFrame(gameLoop);
+                document.getElementById("fondo").style.webkitFilter = "none";
+                
+            }
+            document.getElementById("comenzar").addEventListener("click", function(e) {
+                document.getElementById('inicio').style.visibility = 'hidden';
+                document.getElementById('comenzar').style.visibility = 'hidden';
+                e.preventDefault(); 
+                start();
+            })
+            document.getElementById("volverajugar").addEventListener("click", function(e) {
+                document.getElementById('gameover').style.visibility = 'hidden';
+                document.getElementById('volverajugar').style.visibility = 'hidden';
+                $(".dinosaurioMuerto").removeClass("dinosaurioMuerto").addClass("dinosaurioCorrer"); 
+                puntaje = 0;
+                vidas = 3;
+                document.getElementById('puntaje').style.visibility = 'visible'; 
+                //document.getElementById('vida1').style.display = 'show'; 
+                //document.getElementById('vida2').style.display = 'show'; 
+                //document.getElementById('vida3').style.display = 'show'; 
+                $('#vida1').show();
+                $('#vida2').show();
+                $('#vida3').show();
+                start();
+            })
             
-            
-            
-            
+            document.getElementById('dinosaurio').style.webkitAnimationPlayState = 'paused';
+            document.getElementById('fondo').style.webkitAnimationPlayState = 'paused';
+            document.getElementById('roca').style.webkitAnimationPlayState = 'paused'; 
+            document.getElementById('puntaje').style.webkitAnimationPlayState = 'paused'; 
+            document.getElementById('vida1').style.visibility = 'hidden'; 
+            document.getElementById('vida2').style.visibility = 'hidden'; 
+            document.getElementById('vida3').style.visibility = 'hidden'; 
+            document.getElementById("fondo").style.webkitFilter = "blur(8px)";
